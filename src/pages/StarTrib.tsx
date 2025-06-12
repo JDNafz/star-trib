ffbf4404ae06bbbc19ffddc06b95ac5d9d34968c
JDNafz <nafziger10@gmail.com>
2025-06-12 01:01:54 -0500

    img and slight scrolled added when not logged in

--- src/pages/StarTrib.tsx
+++ src/pages/StarTrib.tsx
@@ -1,18 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { exampleArticle, snippet } from "./api/exampleArticle";
 //imported the styles on _app.tsx
 
 //source URL: https://www.startribune.com/trump-and-cabinet-officials-cite-minneapolis-riots-walzs-response-to-justify-troops-in-los-angeles/601371144
 
 const article = exampleArticle;
+const loggedOut = { backgroundColor: "red" };
 
 const StarTrib: React.FC = () => {
-  const isLoggedIn: boolean = false;
-  const getPreview = (text: string): string => {
-    // const words = text.split(" ");
-    // return words.slice(0, 50).join(" ");
-    return snippet; // simplified as the article doesn't display the first paragraph, rather the 3rd. I'm sure you have features to choose what to display.
-  };
+  const [isLoggedIn, setIsLogedIn] = useState<Boolean>(false);
 
   return (
     <>
@@ -22,21 +18,35 @@ const StarTrib: React.FC = () => {
       </div>
       <div className="nav2">
         <p>Minnesota News You Can Use</p>
-				<p>Subscribe</p>
+        <p
+          style={isLoggedIn ? {} : loggedOut}
+          onClick={() => setIsLogedIn(!isLoggedIn)}
+        >
+          {isLoggedIn ? "Toggle: isLoggedIn" : "Toggle: !isLoggedIn"}
+        </p>
       </div>
       <div className="politics">POLITICS</div>
       <div className="title">{article.title}</div>
-      <div>
-        {isLoggedIn ? (
-          <p className="editorial-body">{article.body}</p>
-        ) : (
-          <>
-            <p className="editorial-body">{getPreview(article.body)}</p>
-            <div className="overlay"></div>
-          </>
-        )}
-      </div>
+      <p className="editorial-body">{snippet}</p>
+      <div className="bold">By Ryan Faircloth</div>
+      <div className="smallText">The Minnesota Star Tribune</div>
+      <div className="smallText2">June 11, 2025 at 5:18PM</div>
+      <img
+        src={
+          "https://arc.stimg.co/startribunemedia/L5D6BUVRXZBERHB5I2T6S25UJI.jpg?&w=390"
+        }
+      ></img>
 
+      {isLoggedIn ? (
+        <p className="editorial-body">{article.body}</p>
+      ) : (
+        <>
+          <p className="editorial-body fake-text">
+            Sorry, try logging in to view full article.
+          </p>
+        </>
+      )}
+			
       {/* insert paywall pop up */}
     </>
   );
