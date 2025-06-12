939388eda678748f7ab04a9223a44f4ea51246ab
JDNafz <nafziger10@gmail.com>
2025-06-12 00:17:25 -0500

    nav text and css added

--- src/pages/StarTrib.tsx
+++ src/pages/StarTrib.tsx
@@ -1,34 +1,44 @@
 import React from "react";
-import { exampleArticle } from "./exampleArticle";
+import { exampleArticle, snippet } from "./api/exampleArticle";
+//imported the styles on _app.tsx
 
-//source URL: https://www.startribune.com/some-people-wont-be-charged-in-feeding-our-future-uncharged/601368099
-
+//source URL: https://www.startribune.com/trump-and-cabinet-officials-cite-minneapolis-riots-walzs-response-to-justify-troops-in-los-angeles/601371144
 
 const article = exampleArticle;
 
 const StarTrib: React.FC = () => {
   const isLoggedIn: boolean = false;
-  const getPreview = (text:string):string => {
-		const words = text.split(' ');
-		return words.slice(0,36).join(' '); // 36 words is the firs paragraph in this particular article, clearly you have logic to do much of this already.
-	}
+  const getPreview = (text: string): string => {
+    // const words = text.split(" ");
+    // return words.slice(0, 50).join(" ");
+    return snippet; // simplified as the article doesn't display the first paragraph, rather the 3rd. I'm sure you have features to choose what to display.
+  };
 
   return (
-		<>
-			{/* insert title */}
-    <div>
-		{isLoggedIn ? (
-			<p className="articleBody">{article.body}</p>
-		) : (
-			<>
-          <p>{getPreview(article.body)}</p>
-          <div className="overlay"></div>
-        </>
-      )}
-			</div>
+    <>
+      <div className="nav1">
+        * The Minnesota Star Tribune
+        {/* Imagine if I replicated the entire website */}
+      </div>
+      <div className="nav2">
+        <p>Minnesota News You Can Use</p>
+				<p>Subscribe</p>
+      </div>
+      <div className="politics">POLITICS</div>
+      <div className="title">{article.title}</div>
+      <div>
+        {isLoggedIn ? (
+          <p className="editorial-body">{article.body}</p>
+        ) : (
+          <>
+            <p className="editorial-body">{getPreview(article.body)}</p>
+            <div className="overlay"></div>
+          </>
+        )}
+      </div>
 
-			{/* insert paywall pop up */}
-		</>
+      {/* insert paywall pop up */}
+    </>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
-import Head from "next/head";
-import Image from "next/image";
+
 import { Geist, Geist_Mono } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 import StarTrib from "./StarTrib";
+
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
